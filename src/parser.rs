1ac6d9a4e18cb792986627429c01931ac860ef6f
Kenneth Salanga <kennethsalanga@gmail.com>
2023-01-13 03:43:15 -0500

    Optimal Bracket Pair Finding Algorithm
    Gets us:
    1. Bracket Pair Indices
    2. Bracket Tree Hierarchy
    3. cleans out any incorrect bracket formations
    in O(N) time where N is the size of string

--- src/parser.rs
+++ src/parser.rs
@@ -7,71 +7,43 @@ use std::str::FromStr;
 
 // Initial Notes:
 
-// First: to clean out any incorrectly nested brackets:
-// - make a stack of opening brackets: [ and closing brackets: ]
-// - if those stacks are unequal lengths:
-//      - there isn't a closing bracket for every opening bracket.
-//      - that is an incorrectly nested group of brackets.
+// Optimal Bracket algorithm that gets us:
+// - the bracket pair indices
+// - the bracket hierarchy tree
+// - cleans out any incorrect bracket formations
 
-// How can we match an opening bracket to its respective closing bracket?
+// in O(N) time where N is the size of the string
 
-// Finding all matching bracket pairs: Assuming we have valid amount of brackets (all opening matches a closing):
-// - Starting from the inner most opening bracket:
-//      - An opening bracket's closing bracket pair, is the closest closing bracket index THAT HASN'T BEEN SEEN to its right.
-//      - put that closing bracket index into a seen index set
-//      - continue on to the next innermost bracket
-// - This algorithm works, might not be the most efficient
-// - This gets us all bracket pairs, but it doesn't show the bracket hierarchy
+// Observations:
+// 1. Closing bracket ALWAYS pairs with the innermost opening bracket up to that point.
+//      - it can't get the second innermost or third innermost up to that point because:
+//      - if we get anything other than the inner most bracket, we get [[], [[[], etc. this is an invalid bracket pair.
+// 2. Any bracket has one or no parent
+// 3. Any bracket has zero or more children
 
-// What brackets are inside other brackets?
-// What brackets are siblings to other brackets?
+// what our algorithm is going to do is:
+// create an opening brackets stack.
 
-// 1. a bracket has a single parent
-// 2. a bracket can have multiple children
+// starting from the left of the string going right until end:
+// - keep adding opening brackets to the stack
+// - if we encounter a closing bracket:
+//      - if the stack is empty:
+//          - INVALID bracket string.
+//          - Closing bracket must always accompany an innermost opening bracket up to that point.
+//      - else pop the top of the opening bracket stack:
+//          - From our observation: this is the inner most bracket up to that point of the closing bracket
+//          - This gets us two important things:
+//              1. The starting and ending index for this bracket pair
+//              2. We know that the parent of this bracket pair is the next innermost opening bracket index from the stack
+//                  - if the next innermost is empty: then the parent is the root
+//                  - else: peak the top and make that opening bracket index its parent
+//                  - for a bracket pair to be a child of some outer bracket, it must be exactly one inner level from the outer bracket.
+//                  - the stack exactly shows us this relationship.
+//                  - up to this point of the bracket pair:
+//                      - the innermost opening bracket is the child, and the second innermost is its one outer level parent.
 
-// You are a parent of a bracket pair if:
-// your bracket pair overlaps the other bracket pair
-
-// you are a child of a parent if:
-// your bracket pair is inside some bracket pair
-
-// This algorithm does not work. counter example:
-// [[], [], []], [[], [], []]
-// Creating bracket pair hierarchy: Initial Algorithm
-// - Starting from the innermost opening bracket, bracket pair as the Current Level Bracket Pair
-// - Create a siblings / same level vector
-// - While the bracket pairs are not empty,
-//      - Pop the next innermost opening bracket, bracket pair:
-//          - if popped bracket isn't their parent, they're siblings:
-//              - add this popped bracket pair into the siblings vector
-//          - if it is their parent:
-//              - set all aggregated siblings + the current bracket pair as children to that parent
-//              - make the current level bracket pair to this parent. "moved up the tree"
-
-// Note: Treat bracket pairs as similar to intervals with an (opening bracket, closing bracket) being a (starting index, ending index) in the string
-
-// Another tree generation algorithm idea:
-
-// Do this for ALL interval pairs:
-// Note: We assume we already have the interval pairs so accessing the corresponding closing bracket for every opening bracket index is constant time.
-// For every bracket pair = current bracket pair:
-// - Make this interval a node and store the key: interval value: node reference in a hashmap if it doesn't exist in the hashmap
-// - find the first opening bracket to its left that current bracket is inside of:
-//      - first opening bracket = parent:
-//          - if parent interval is already in hashmap:
-//              - parent_node = the node reference in the hashmap.
-//          - else:
-//              - parent_node = make a new node for parent, store it in hashmap.
-//          - current bracket parent = parent_node
-//          - parent_node child = current_bracket
-//      - if you cant find any to the left that it fits in:
-//          - its parent is the root bracket
-// - move to the next bracket interval
-
-// After all parent child relationships have been made:
-// - Starting from the root, reconstruct the bracket tree / hierarchy.
-
-// O(N^2) solution, for every bracket pair, we might look at every other bracket pair.
+// - if opening brackets stack is not empty: invalid bracket string.
+//      - every opening bracket has a close bracket pair
 
 pub struct PacketParserError;
 

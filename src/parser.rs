bad9732bca0218d9fa2c2677e35932953b3fc97d
Kenneth Salanga <kennethsalanga@gmail.com>
2023-01-12 21:34:51 -0500

    Finding Bracket Pairs for a String Done
    - Might not be the most efficient algo, but it is correct

--- src/parser.rs
+++ src/parser.rs
@@ -4,6 +4,16 @@ use std::str::FromStr;
 /// into a Vector of PacketDatums
 /// author: Kenneth Salanga
 
+// Initial Notes:
+
+// How can we match an opening bracket to its respective closing bracket?
+
+// Finding all matching bracket pairs: Assuming we have valid amount of brackets (all opening matches a closing):
+// - Starting from the inner most opening bracket:
+//      - An opening bracket's closing bracket pair, is the closest closing bracket index THAT HASN'T BEEN SEEN to its right.
+//      - put that closing bracket index into a seen index set
+//      - continue on to the next innermost bracket
+// - This algorithm works, might not be the most efficient
 pub struct PacketParserError;
 
 pub struct PacketParser {}

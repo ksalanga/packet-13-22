8320ba2f4d9d48d6ea9caea54115397cdea2c14a
Kenneth Salanga <kennethsalanga@gmail.com>
2023-01-13 19:42:11 -0500

    Modified Optimal Parsing Algorithm
    Parsing list string no longer creates a tree with just bracket nodes
    
    Tree Structure now represented as the PacketDatum nested Enum itself
    
    Tweaked Algorithm can now allow Integers
    
    After parsing: gets us the Root PacketDatum List @ hashmap index 0
    - Assumes: string starts off as a list. ex: "[ ]"

--- src/parser.rs
+++ src/parser.rs
@@ -21,34 +21,56 @@ use std::str::FromStr;
 // 2. Any bracket has one or no parent
 // 3. Any bracket has zero or more children
 
-// what our algorithm is going to do is:
-// create an opening brackets stack.
+// How we can include ints into the tree hierarchy while making our PacketDatum Structures
 
-// starting from the left of the string going right until end:
-// - keep adding opening brackets to the stack
-// - if we encounter a closing bracket:
-//      - if the stack is empty:
+// Instead of creating a tree structure with just the "bracket pair" nodes,
+// We parse the characters in the strings and build the nested structure from the PacketDatum enum.
+
+// PacketDatum enum already contains Variants:
+// - List
+// - Integer
+// so just create the nested "tree" structure from that.
+// this will now also include Integers instead of just brackets / List variant.
+
+// New Parsing Algorithm that includes ints:
+
+// Assumes: string starts off as a list. ex: "[ ]"
+// if string doesn't start off with [, abort: invalid list string
+
+// Create an opening brackets index stack: Stack<usize> which represents the opening bracket index
+// Create a list hashmap: HashMap<usize, Rc<RefCell<PacketDatum::List>>:
+// - List can have parent lists so we need
+// - quick access to all of the List PacketDatums
+// - so that a parent list can add a child list to its vector of "children"
+
+// For each enumerated character in the input string:
+//  if char is an opening bracket:
+//  - we've encountered a new PacketDatum List
+//  - if the bracket stack is empty: this is the root list
+//      - put char index key + new Rc<RefCell<PacketDatum::List> into list hashmap
+//  - else peek the top: the new char is a child PacketDatum List and the top of the stack is its outer parent List
+//      - create new ref list: Rc<RefCell<PacketDatum::List>
+//      - put char index key + ref list value into list hashmap
+//      - get the parent List reference from the <index, PacketDatum> hashmap
+//          - make a copy of ref list and add it as a child to the parent list
+//  - put char index into opening brackets index stack
+//  if char is an int:
+//  - if the stack is empty: INVALID Bracket String. integer should always be inside a list
+//  - else: peek the top of the opening bracket index stack. that is the int's parent
+//      - get the parent List ref from the <index, PacketDatum> hashmap
+//      - add the child Integer PacketDatum to the Parent List
+//  if char is a closing bracket:
+//      - if stack is empty:
 //          - INVALID bracket string.
 //          - Closing bracket must always accompany an innermost opening bracket up to that point.
-//      - else pop the top of the opening bracket stack:
-//          - From our observation: this is the inner most bracket up to that point of the closing bracket
-//          - This gets us two important things:
-//              1. The starting and ending index for this bracket pair
-//              2. We know that the parent of this bracket pair is the next innermost opening bracket index from the stack
-//                  - if the next innermost is empty: then the parent is the root
-//                  - else: peak the top and make that opening bracket index its parent
-//                  - for a bracket pair to be a child of some outer bracket, it must be exactly one inner level from the outer bracket.
-//                  - the stack exactly shows us this relationship.
-//                  - up to this point of the bracket pair:
-//                      - the innermost opening bracket is the child, and the second innermost is its one outer level parent.
+//      - else:
+//          - pop the opening brackets index stack.
+//          - that is the end (no more PacketDatum items) of that list where the opening bracket is @ the popped index
 
-// - if opening brackets stack is not empty: invalid bracket string.
-//      - every opening bracket has a close bracket pair
+// if opening brackets stack is not empty: invalid bracket string.
+//  - every opening bracket has a close bracket pair
 
-// TODO: Bracket Pair Hierarchy Forming Algorithm does include ints
-// For any bracket with children:
-// the children are only brackets and do not include potential ints inside
-// Once we include ints, we have the actual PacketDatum / Item Tree
+// This should get us the "Tree Hierarchy" where the root PacketDatum List is in the hashmap @ index 0
 
 pub struct PacketParserError;
 
